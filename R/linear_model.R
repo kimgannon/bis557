2b017209629c87f8a57a9b8a05633cdab7b47963
kimgannon <gannonkim0@gmail.com>
2020-09-09 16:09:52 -0400

    Added tests, modified function accordingly

--- R/linear_model.R
+++ R/linear_model.R
@@ -4,17 +4,17 @@
 #' a list of factor variables.
 #' @param formula the model to which data will be fit
 #' @param df the data frame containing data to be fit
-#' @param contrasts_list a list of variable names to be used as factor variables
+#' @param contrasts a list of variable names to be used as factor variables
 #' @export
 #' @examples
 #' library(palmerpenguins)
 #' data("penguins")
 #' penguinsform <- bill_length_mm ~ body_mass_g
 #' penguinsdf <- (as.data.frame(penguins))
-#' penguinscont <- list("species", "year")
-#' linear_model(penguinsform, penguinsdf, penguinscont)
+#' penguinscont <- list(Species = "contr.sum")
+#' linear_model(penguinsform, df=penguinsdf, contrasts=penguinscont)
 
 
-linear_model <- function(formula, df, contrasts_list) {
-  stats::lm(formula, data=df, contrasts.arg=contrasts_list)
+linear_model <- function(formula, df, contrasts=NULL) {
+  stats::lm(formula, data=df, contrasts=contrasts)
 }
---
+++ R/lm_patho-doc.R
@@ -0,0 +1,8 @@
+#' @title lm_patho-doc
+#' @name lm_patho
+#' @rdname lm_patho
+#' @docType data
+#' @description
+#' The lm_patho raw data set converted to .rda format
+#' @keywords data
+NULL
