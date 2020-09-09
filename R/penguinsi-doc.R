90bd645b8f973f82626f30473442ff3ba63b4b30
kimgannon <gannonkim0@gmail.com>
2020-09-09 04:00:24 -0400

    changed Rbuildignore and documentation

--- R/penguinsi-doc.R
+++ R/penguinsi-doc.R
@@ -1,6 +1,7 @@
+#' @title penguinsi-doc
 #' @name penguinsi
-#' @docType data
 #' @rdname penguinsi
+#' @docType data
 #' @description
 #' The penguins data set after imputing all missing values using the package missForest
 #' @references See the palmerpenguins::penguins documentation for information about the values
